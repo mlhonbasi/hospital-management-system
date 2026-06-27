67a33230b092772b81845a226b3ec0447dad98c1
mlhonbasi <melihonbasi16@gmail.com>
2026-06-27 23:00:52 +0300

    feat(appointment-service): extend automapper with AppointmentDetail DTO configurations

--- Microservices/HospitalManagementSystem.AppointmentService/Mapping/GeneralMapping.cs
+++ Microservices/HospitalManagementSystem.AppointmentService/Mapping/GeneralMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HospitalManagementSystem.AppointmentService.Dtos.AppointmentDtos;
+using HospitalManagementSystem.AppointmentService.Dtos.AppointmetDetailDtos;
 using HospitalManagementSystem.AppointmentService.Entitites;
 
 namespace HospitalManagementSystem.AppointmentService.Mapping
@@ -12,6 +13,11 @@ namespace HospitalManagementSystem.AppointmentService.Mapping
             CreateMap<Appointment, GetAppointmentByIdDto>().ReverseMap();
             CreateMap<Appointment, ResultAppointmentDto>().ReverseMap();
             CreateMap<Appointment, UpdateAppointmentDto>().ReverseMap();
+
+            CreateMap<AppointmentDetail, CreateAppointmentDetailDto>().ReverseMap();
+            CreateMap<AppointmentDetail, UpdateAppointmentDetailDto>().ReverseMap();
+            CreateMap<AppointmentDetail, ResultAppointmentDetailDto>().ReverseMap();
+            CreateMap<AppointmentDetail, GetAppointmentDetailById>().ReverseMap();
         }
     }
 }
