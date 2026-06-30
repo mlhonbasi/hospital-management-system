26c79472a2e05f9926565e73881485a94fbb6c3c
mlhonbasi <melihonbasi16@gmail.com>
2026-06-30 14:44:12 +0300

    feat(appointment-service): add AppointmentDetailsController

---
+++ Microservices/HospitalManagementSystem.AppointmentService/Controllers/AppointmentDetailsController.cs
@@ -0,0 +1,32 @@
+﻿using HospitalManagementSystem.AppointmentService.Dtos.AppointmetDetailDtos;
+using HospitalManagementSystem.AppointmentService.Services.AppointmentDetailServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.AppointmentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentDetailsController(IAppointmentDetailService appointmentDetailService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAppointmentDetail(CreateAppointmentDetailDto dto)
+        {
+            await appointmentDetailService.CreateAsync(dto);
+            return Ok("Randevu detayı başarıyla oluşturuldu.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateAppointmentDetail(UpdateAppointmentDetailDto dto)
+        {
+            await appointmentDetailService.UpdateAsync(dto);
+            return Ok("Randevu detayı başarıyla güncellendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAppointmentDetail(int id)
+        {
+            var value = await appointmentDetailService.GetByAppointmentId(id);
+            if (value == null)
+                return NotFound("Randevu detayı bulunamadı.");
+            return Ok(value);
+        }
+    }
+}
