49d0a456259995d919731d66492fb1adcb0be013
mlhonbasi <melihonbasi16@gmail.com>
2026-07-01 17:32:06 +0300

    feat(identity-service): add AppUser model and configure HMSIdentityDbContext with migrations
    - Create AppUser extending IdentityUser
    - Implement HMSIdentityDbContext
    - Generate initial Entity Framework migrations for Identity tables
    - Register database context in Program.cs

---
+++ Microservices/HospitalManagementSystem.IdentityService/Context/HMSIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using HospitalManagementSystem.IdentityService.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagementSystem.IdentityService.Context
+{
+    public class HMSIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public HMSIdentityDbContext(DbContextOptions<HMSIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
